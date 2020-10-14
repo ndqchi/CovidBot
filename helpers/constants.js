3db5717030b88955710347d9ff59a1e187507aa7
jolie <ndqchi1811@gmail.com>
2020-10-15 02:07:30 +0800

    fix export es6

--- helpers/constants.js
+++ helpers/constants.js
@@ -1,15 +1,15 @@
-export const CONFIDENCE_THRESHOLD = 0.8;
-export const encouragingMessage = [
+const CONFIDENCE_THRESHOLD = 0.8;
+const encouragingMessage = [
     "Hey! Don't feel so down! We are all in this together. I'm sure we can overcome it soon!",
     "Don't worry my friend! Everything will turn out well! Let's believe in human capabilities!",
     "Cheer up! I believe that with unity, humans can win against any diseases or evils!"
 ];
-export const fallbackMessage = [
+const fallbackMessage = [
     "Sorry! What was that? Can you try again with something like 'Global covid status' or '<some country> status'?",
     "I don't really understand! :( Mind trying again?",
     "I will always be here for you! Shall we try again?"
 ];
-export const byeMessage = [
+const byeMessage = [
     "Goodbye! Great talk with you! Please stay safe, especially during this Covid19.",
     "Nice time chatting with you! Wish you well during this epidemic! Bye bye!",
     "You have to go now :(? Wish I can talk to you more! See you! Stay safe!",
@@ -17,7 +17,7 @@ export const byeMessage = [
     "Byebye! Hope to see you again. Wish you great health.",
     "Take care! You can come back to me whenever you like! I am always here!"
 ];
-export const welcomeMessage = [
+const welcomeMessage = [
     "Hi there! I am Covid19 chatbot. " +
     "You can ask me about the latest Covid19 situation around the world. " +
     "Some examples to give you a feel: 'Tell me about Covid19 status in <country>/globally', " +
@@ -26,3 +26,10 @@ export const welcomeMessage = [
     "Here are some common questions I get asked: 'Singapore covid19 status', 'What is covid situation in Germany?' or " +
     "'How is covid19 globally?'"
 ];
+module.exports = {
+    CONFIDENCE_THRESHOLD: CONFIDENCE_THRESHOLD,
+    welcomeMessage: welcomeMessage,
+    byeMessage: byeMessage,
+    encouragingMessage: encouragingMessage,
+    fallbackMessage: fallbackMessage
+};
